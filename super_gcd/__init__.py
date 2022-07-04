6fa4c4d25b34bf54a844fa551006954fcc930406
Sirakorn Lamyai <sirakorn.lamyai@adastragrp.com>
2022-07-04 16:38:52 +0700

    Computing GCD through form

--- super_gcd/__init__.py
+++ super_gcd/__init__.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+
+
+def gcd(x, y):
+    """Returns the greatest common denominator of x and y"""
+    # TODO: implement this
+    return -1
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -8,4 +14,11 @@ def main():
     if request.method == "GET":
         return render_template("index.html")
     elif request.method == "POST":
-        return render_template("index.html")
+        try:
+            x = int(request.form["number_1"])
+            y = int(request.form["number_2"])
+            xy_gcd = gcd(x, y)
+            message = f"The GCD is {xy_gcd}."
+        except (KeyError, ValueError):
+            message = "Invalid value!"
+        return render_template("index.html", message=message)
