c4484b6ef6457ab5b00f5cfdb2b5cc27bfbfde37
Sirakorn Lamyai <sirakorn.lamyai@adastragrp.com>
2022-07-04 16:41:00 +0700

    Fallback setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+
+    use_setuptools()
+    from setuptools import setup
+
+setup(setup_cfg=True)
